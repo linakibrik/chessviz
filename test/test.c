93b2f026aadbc7fd097876f9532588883d9fdaa9
Коля Таронтино <Kolya@localhost.localdomain>
2018-05-09 17:34:19 +0600

    test of hours, test.c

--- test/test.c
+++ test/test.c
@@ -40,6 +40,18 @@ CTEST(R_Move, Incorrect) {
     ASSERT_EQUAL(expected,result);
 }
 
+CTEST(H_Move, Correct) {
+    int result = board_func("b1-c3", 1);
+    int expected = 0;
+    ASSERT_EQUAL(expected,result);
+}
+
+CTEST(H_Move, Incorrect) {
+    int result = board_func("b1-b3", 1);
+    int expected = -1;
+    ASSERT_EQUAL(expected,result);
+}
+
 int main(int argc, const char** argv) {
     _board();
     return ctest_main(argc, argv);
