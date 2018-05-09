09bb7a507e44c0838bcb0abc4f1b51be93b1b9ec
Коля Таронтино <Kolya@localhost.localdomain>
2018-05-09 18:44:01 +0600

    4 version chess.c

--- src/chess.c
+++ src/chess.c
@@ -37,50 +37,60 @@ void print_new_board()
 
 void print_board(char **arr)
 {
-   system("clear");
-   printf("\n");
-   for (int i = 0; i < 8; i++) {
+	system("clear");
+    printf("\n");
+    for (int i = 0; i < 8; i++) {
         printf("\t%d", 8 - i);
         for (int j = 0; j < 8; j++) {
             printf("%c", container[i][j]);
         }
         printf("\n");
     }
+    
+    printf("\t");
+    
     for (char p = 'a'; p <= 'h'; p++) {
         printf("%c", p);
     }
+    
+    printf("\n");
 }
 
 char** movePawn(char **v, int* pozition)
 {
-    if  (pozition[0] == pozition[2]) {
-        int poz = arr[pozition[1]][pozition[0]];
-        arr[pozition[1]][pozition[0]] = arr[pozition[3]][pozition[2]];
-        arr[pozition[3]][pozition[2]] = poz;
-    } 
-
-    else printf ("Try again");
-
+    int poz = arr[pozition[1]][pozition[0]];
+    arr[pozition[1]][pozition[0]] = arr[pozition[3]][pozition[2]];
+    arr[pozition[3]][pozition[2]] = poz;
+   
     return v;
 }
 
-int board_func(char *one_place, char *two_place) {
 
+int board_func(char *places, int test) 
+{
+
+    if (strlen(places) != 5) {
+        return -1;
+    }
     int pozition[4];
-    pozition[0] = one_place[0] - 96; //a
-    pozition[1] = 9 - (one_place[1] - 48) - 1; //2
-    pozition[2] = two_place[0] - 96; //c
-    pozition[3] = 9 - (two_place[1] - 48) - 1; //8
+    pozition[0] = places[0] - 96 - 1; //a
+    pozition[1] = 9 - (places[1] - 48) - 1; //2
+    pozition[2] = places[3] - 96 - 1; //c
+    pozition[3] = 9 - (places[4] - 48) - 1; //8
     printf("\n ");
+    int check = move_fig(pozition, container);
+    if (check == -1) { 
+		return -1; 
+	}
 
     for(int g = 0; g < 4; g++) {
-        if (pozition[g] < 0 || pozition[g] > 9) {
+        if (pozition[g] < 0 || pozition[g] > 8) {
             return -1;
         }
     }
-
     arr = movePawn(arr, pozition);
-    print_board(arr);
+    if (test == 0) {
+        print_board();
+    }
     return 0;
 }
-
