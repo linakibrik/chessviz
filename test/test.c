94a78d0bba467bb3ae0524fb05ffc529dd82a45a
Коля Таронтино <Kolya@localhost.localdomain>
2018-05-09 17:21:11 +0600

    test of sintax, test.c

---
+++ test/test.c
@@ -0,0 +1,23 @@
+#define CTEST_MAIN
+
+#include <board.h>
+#include <check_strokes.h>
+#include <ctest.h>
+
+CTEST(Syntax, Correct_Syntax) {
+    int result = board_func("e2-e4", 1);
+    int expected = 0;
+    ASSERT_EQUAL(expected,result);
+}
+
+CTEST(Syntax, Incorrect_Syntax) {
+    int result = board_func("nb1-a3", 1);
+    int expected = -1;
+    ASSERT_EQUAL(expected,result);
+}
+
+
+int main(int argc, const char** argv) {
+    _board();
+    return ctest_main(argc, argv);
+}
